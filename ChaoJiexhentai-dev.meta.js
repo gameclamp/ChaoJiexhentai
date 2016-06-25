1a06177ee6246fae711add7379945549a14a5448
9尾雪狐 <gameclamp@gmail.com>
2016-06-25 13:21:50 +0800

    Create new file

---
+++ ChaoJiexhentai-dev.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        ChaoJiexhentai-dev
+// @namespace   https://github.com/gameclamp/ChaoJiexhentai
+// @homepageURL https://github.com/gameclamp/ChaoJiexhentai
+// @supportURL  https://github.com/gameclamp/ChaoJiexhentai/issues
+// @author      9尾雪狐(gameclamp)
+// @description 更好的看漫画，更多帮助查看https://github.com/gameclamp/ChaoJiexhentai
+// @icon        https://github.com/gameclamp/ChaoJiexhentai/raw/master/icon.png
+// @include     http://exhentai.org/g/*
+// @include     http://g.e-hentai.org/g/*
+// @downloadURL https://github.com/gameclamp/ChaoJiexhentai/raw/master/ChaoJiexhentai-dev.user.js
+// @updateURL   https://github.com/gameclamp/ChaoJiexhentai/raw/master/ChaoJiexhentai-dev.meta.js
+// @version     0.3.4
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
