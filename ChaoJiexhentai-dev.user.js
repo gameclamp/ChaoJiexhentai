8d3d5f2f884a8890a736b9d41f71020da688adcd
9尾雪狐 <gameclamp@gmail.com>
2016-07-29 00:25:25 +0800

    Update ChaoJiexhentai-dev.user.js
    exhentai->https

--- ChaoJiexhentai-dev.user.js
+++ ChaoJiexhentai-dev.user.js
@@ -6,11 +6,11 @@
 // @author      9尾雪狐(gameclamp)
 // @description 更好的看漫画，更多帮助查看https://github.com/gameclamp/ChaoJiexhentai
 // @icon        https://github.com/gameclamp/ChaoJiexhentai/raw/master/icon.png
-// @include     http://exhentai.org/g/*
+// @include     https://exhentai.org/g/*
 // @include     http://g.e-hentai.org/g/*
 // @downloadURL https://github.com/gameclamp/ChaoJiexhentai/raw/master/ChaoJiexhentai-dev.user.js
 // @updateURL   https://github.com/gameclamp/ChaoJiexhentai/raw/master/ChaoJiexhentai-dev.meta.js
-// @version     0.3.4
+// @version     0.3.5
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
 var $a = function(a){return document.querySelector(a)};
